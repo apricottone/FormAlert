3498c995459a0e59b7da9610b629ac44f2c70182
MizHiro <pencil3274@gmail.com>
2021-11-17 18:07:44 +0900

    implementation of functions

--- DisplayAlert/ViewController.swift
+++ DisplayAlert/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func alert(_ sender: Any) {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+        alert.title = "ここにタイトル"
+        alert.message = "メッセージ"
+        
+        alert.addTextField(configurationHandler: {(textField) -> Void in
+            textField.delegate = self
+            
+        })
+        
+        //追加ボタン
+        alert.addAction(
+            UIAlertAction(
+                title: "追加",
+                style: .default,
+                handler: {(action) -> Void in
+                    self.hello(action.title!)
+                })
+        )
+        
+        //キャンセルボタン
+        alert.addAction(
+            UIAlertAction(
+                title: "キャンセル",
+                style: .cancel,
+                handler: {(action) -> Void in
+                    self.hello(action.title!)
+                })
+        )
+        
+        //アラートが表示されるごとにprint
+        self.present(
+            alert,
+            animated: true,
+            completion: {
+                print("アラートが表示された")
+            })
+    }
+    
+    func hello(_ msg:String){
+        print(msg)
+    }
+    
 }
+
+
 
